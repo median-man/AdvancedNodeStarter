451c18b43101575f1a76b7d6dcf95f4db4c7f103
John Desrosiers <john.desro@gmail.com>
2019-06-14 04:36:32 -0700

    Removes cacheing from GET /api/blogs/:id

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,29 +5,12 @@ const Blog = mongoose.model('Blog');
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const REDIS_URL = 'redis://127.0.0.1:6379'
-    const redis = require('redis')
-    const util = require('util')
-    const redisClient = redis.createClient(REDIS_URL)
-    redisClient.monitor(console.log)
-    redisClient.get = util.promisify(redisClient.get)
-    
-    const cachedBlogs = await redisClient.get(req.user.id)
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHE', new Date())
-      res.send(JSON.parse(cachedBlogs))
-      return
-    }
-
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
     });
 
-    console.log('SERVING FROM MONGODB', new Date())
     res.send(blog);
-    
-    redisClient.set(req.user.id, JSON.stringify(blog))
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
