703b101dc95e453ebeafebb9be35762e5b8ba2ee
John Desrosiers <john.desro@gmail.com>
2019-06-14 05:57:33 -0700

    Basic caching applied to mongoose Query exec
    This adds basic caching behavior to Query#exec method of mongoose.
    
    IMPORTANT CAVEATS:
    
    - NO EXPIRATIONS HAVE BEEN CONFIGURED YET
    - DOES NOT EXPIRE CACHED DATA ON WRITES TO DB

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,38 @@
 const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
 
+const REDIS_URI = 'redis://127.0.0.1:6379'
+
+const createRedisClient = () => {
+  const client = redis.createClient(REDIS_URI)
+  client.get = util.promisify(client.get)
+  client.set = util.promisify(client.set)
+  return client
+}
 const { exec } = mongoose.Query.prototype
 
-mongoose.Query.prototype.exec = function() {
-  console.log('cache: EXECUTING QUERY')
-  return exec.apply(this, arguments)
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify({
+    ...this.getQuery(),
+    collection: this.mongooseCollection.name
+  })
+  console.log('cache: EXECUTING QUERY', key)
+
+  // check the cache for the key
+  const client = createRedisClient()
+
+  const cacheValue = await client.get(key)
+  if (cacheValue) {
+    console.log('cache: RETURNING CACHE VALUE', cacheValue)
+    const doc = JSON.parse(cacheValue)
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc)
+  }
+
+  const queryResult = await exec.apply(this, arguments)
+  console.log('cache: RETURNING MONGO QUERY RESULT', queryResult)
+  client.set(key, JSON.stringify(queryResult))
+  return queryResult
 }
